d13d6ccbcc080c574c412e1df11ffdfdb11e6751
Emil Bay <github@tixz.dk>
2020-04-27 13:42:41 +0200

    Move out request

--- index.js
+++ index.js
@@ -1,185 +1,186 @@
+class Request {
+  constructor (rpc, method, args = []) {
+    this.rpc = rpc
+    this.method = method
+    this.args = args
+  }
+
+  then () {
+    return this.rpc.request(this.method, this.args)
+  }
+}
+
 module.exports = class ETH {
   constructor (rpc) {
     this.rpc = rpc
-
-    this.Request = class Request {
-      constructor (method, args = []) {
-        this.method = method
-        this.args = args
-      }
-
-      then () {
-        return rpc.request(this.method, this.args)
-      }
-    }
   }
 
   subscribe (req, cb) {
-    return this.rpc.subscribe(req.method, req.args, cb)
+    return req.rpc.subscribe(req.method, req.args, cb)
   }
 
   accounts () {
-    return new this.Request('eth_accounts', [])
+    return new Request('eth_accounts', [])
   }
 
   blockNumber () {
-    return new this.Request('eth_blockNumber', [])
+    return new Request('eth_blockNumber', [])
   }
 
   call (obj, from) {
-    return new this.Request('eth_call', from ? [obj, from] : [obj])
+    return new Request('eth_call', from ? [obj, from] : [obj])
   }
 
   chainId () {
-    return new this.Request('eth_chainId', [])
+    return new Request('eth_chainId', [])
   }
 
   coinbase () {
-    return new this.Request('eth_coinbase', [])
+    return new Request('eth_coinbase', [])
   }
 
   estimateGas (obj, from) {
-    return new this.Request('eth_estimateGas', from ? [obj, from] : [obj])
+    return new Request('eth_estimateGas', from ? [obj, from] : [obj])
   }
 
   gasPrice () {
-    return new this.Request('eth_gasPrice', [])
+    return new Request('eth_gasPrice', [])
   }
 
   getBalance (obj, from) {
-    return new this.Request('eth_getBalance', from ? [obj, from] : [obj])
+    return new Request('eth_getBalance', from ? [obj, from] : [obj])
   }
 
   getBlockByHash (hash, tx) {
-    return new this.Request('eth_getBlockByHash', [hash, tx || false])
+    return new Request('eth_getBlockByHash', [hash, tx || false])
   }
 
   getBlockByNumber (n, tx) {
-    return new this.Request('eth_getBlockByNumber', [n, tx || false])
+    return new Request('eth_getBlockByNumber', [n, tx || false])
   }
 
   getBlockTransactionCountByHash (hash) {
-    return new this.Request('eth_getBlockTransactionCountByHash', [hash])
+    return new Request('eth_getBlockTransactionCountByHash', [hash])
   }
 
   getBlockTransactionCountByNumber (n) {
-    return new this.Request('eth_getBlockTransactionCountByNumber', [n])
+    return new Request('eth_getBlockTransactionCountByNumber', [n])
   }
 
   getCode (addr, from) {
-    return new this.Request('eth_getCode', from ? [addr, from] : [addr])
+    return new Request('eth_getCode', from ? [addr, from] : [addr])
   }
 
   getFilterChanges (id) {
-    return new this.Request('eth_getFilterChanges', [id])
+    return new Request('eth_getFilterChanges', [id])
   }
 
   getFilterLogs (id) {
-    return new this.Request('eth_getFilterLogs', [id])
+    return new Request('eth_getFilterLogs', [id])
   }
 
   getLogs (obj) {
-    return new this.Request('eth_getLogs', [obj])
+    return new Request('eth_getLogs', [obj])
   }
 
   getStorageAt (addr, pos, from) {
-    return new this.Request('eth_getStorageAt', from ? [addr, pos, from] : [addr, pos])
+    return new Request('eth_getStorageAt', from ? [addr, pos, from] : [addr, pos])
   }
 
   getTransactionByBlockHashAndIndex (hash, pos) {
-    return new this.Request('eth_getTransactionByBlockHashAndIndex', [hash, pos])
+    return new Request('eth_getTransactionByBlockHashAndIndex', [hash, pos])
   }
 
   getTransactionByBlockNumberAndIndex (hash, pos) {
-    return new this.Request('eth_getTransactionByBlockNumberAndIndex', [hash, pos])
+    return new Request('eth_getTransactionByBlockNumberAndIndex', [hash, pos])
   }
 
   getTransactionByHash (hash) {
-    return new this.Request('eth_getTransactionByHash', [hash])
+    return new Request('eth_getTransactionByHash', [hash])
   }
 
   getTransactionCount (addr, from) {
-    return new this.Request('eth_getTransactionCount', from ? [addr, from] : [addr])
+    return new Request('eth_getTransactionCount', from ? [addr, from] : [addr])
   }
 
   getTransactionReceipt (hash) {
-    return new this.Request('eth_getTransactionReceipt', [hash])
+    return new Request('eth_getTransactionReceipt', [hash])
   }
 
   getUncleByBlockHashAndIndex (hash, pos) {
-    return new this.Request('eth_getUncleByBlockHashAndIndex', [hash, pos])
+    return new Request('eth_getUncleByBlockHashAndIndex', [hash, pos])
   }
 
   getUncleByBlockNumberAndIndex (n, pos) {
-    return new this.Request('eth_getUncleByBlockNumberAndIndex', [n, pos])
+    return new Request('eth_getUncleByBlockNumberAndIndex', [n, pos])
   }
 
   getUncleCountByBlockHash (hash) {
-    return new this.Request('eth_getUncleCountByBlockHash', [hash])
+    return new Request('eth_getUncleCountByBlockHash', [hash])
   }
 
   getUncleCountByBlockNumber (hash) {
-    return new this.Request('eth_getUncleCountByBlockNumber', [hash])
+    return new Request('eth_getUncleCountByBlockNumber', [hash])
   }
 
   getWork () {
-    return new this.Request('eth_getWork', [])
+    return new Request('eth_getWork', [])
   }
 
   hashrate () {
-    return new this.Request('eth_hashrate', [])
+    return new Request('eth_hashrate', [])
   }
 
   mining () {
-    return new this.Request('eth_mining', [])
+    return new Request('eth_mining', [])
   }
 
   newBlockFilter () {
-    return new this.Request('eth_newBlockFilter', [])
+    return new Request('eth_newBlockFilter', [])
   }
 
   newFilter (obj) {
-    return new this.Request('eth_newFilter', [obj])
+    return new Request('eth_newFilter', [obj])
   }
 
   newPendingTransactionFilter () {
-    return new this.Request('eth_newPendingTransactionFilter', [])
+    return new Request('eth_newPendingTransactionFilter', [])
   }
 
   protocolVersion () {
-    return new this.Request('eth_protocolVersion', [])
+    return new Request('eth_protocolVersion', [])
   }
 
   sendRawTransaction (data) {
-    return new this.Request('eth_sendRawTransaction', [data])
+    return new Request('eth_sendRawTransaction', [data])
   }
 
   sendTransaction (data) {
-    return new this.Request('eth_sendTransaction', [data])
+    return new Request('eth_sendTransaction', [data])
   }
 
   sign (addr, data) {
-    return new this.Request('eth_sign', [addr, data])
+    return new Request('eth_sign', [addr, data])
   }
 
   signTransaction (obj) {
-    return new this.Request('eth_signTransaction', [obj])
+    return new Request('eth_signTransaction', [obj])
   }
 
   submitHashrate (a, b) {
-    return new this.Request('eth_submitHashrate', [a, b])
+    return new Request('eth_submitHashrate', [a, b])
   }
 
   submitWork (a, b, c) {
-    return new this.Request('eth_submitWork', [a, b, c])
+    return new Request('eth_submitWork', [a, b, c])
   }
 
   syncing () {
-    return new this.Request('eth_syncing', [])
+    return new Request('eth_syncing', [])
   }
 
   uninstallFilter () {
-    return new this.Request('eth_uninstallFilter', [])
+    return new Request('eth_uninstallFilter', [])
   }
 
   end () {
